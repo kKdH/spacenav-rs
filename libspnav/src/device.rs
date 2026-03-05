2752e7b1f9e174b9662ab3cb2169fb3937624675
Elmar Schug <elmar.schug@jayware.org>
2026-03-05 21:09:52 +0100

    Added the type of device to the `Device` struct.

--- libspnav/src/device.rs
+++ libspnav/src/device.rs
@@ -1,6 +1,75 @@
+use std::fmt::Formatter;
+
 #[derive(Debug, Clone)]
 pub struct Device {
     pub name: String,
+    pub ty: DeviceType,
     pub axes: u32,
     pub buttons: u32,
+}
+
+#[derive(Debug, Clone)]
+pub enum DeviceType {
+    Unknown,
+    /// Spaceball 1003/2003/2003C
+    Spaceball2003,
+    /// Spaceball 3003/3003C
+    Spaceball3003,
+    /// Spaceball 4000FLX/5000FLX
+    Spaceball4000,
+    MagellanSpaceMouse,
+    /// Spaceball 5000 (spacemouse protocol)
+    Spaceball5000Serial,
+    /// 3Dconnexion CadMan (spacemouse protocol)
+    ConnexionCadManSerial,
+    SpaceMousePlusXT,
+    /// 3Dconnexion CadMan (USB version)
+    ConnexionCadManUsb,
+    SpaceMouseClassic,
+    /// Spaceball 5000 (USB version)
+    Spaceball5000Usb,
+    SpaceTraveller,
+    SpacePilot,
+    SpaceNavigator,
+    SpaceExplorer,
+    SpaceNavigatorForNotebooks,
+    SpacePilotPro,
+    SpaceMousePro,
+    NuLOOQ,
+    SpaceMouseWireless,
+    SpaceMouseProWireless,
+    SpaceMouseEnterprise,
+    SpaceMouseCompact,
+    SpaceMouseModule,
+}
+
+impl std::fmt::Display for DeviceType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DeviceType::Unknown => write!(f, "Unknown"),
+            DeviceType::Spaceball2003 => write!(f, "Spaceball 1003/2003/2003C"),
+            DeviceType::Spaceball3003 => write!(f, "Spaceball 3003/3003C"),
+            DeviceType::Spaceball4000 => write!(f, "Spaceball 4000FLX/5000FLX"),
+            DeviceType::MagellanSpaceMouse => write!(f, "Magellan SpaceMouse"),
+            DeviceType::Spaceball5000Serial => write!(f, "Spaceball 5000 (spacemouse protocol)"),
+            DeviceType::ConnexionCadManSerial => write!(f, "3Dconnexion CadMan (spacemouse protocol)"),
+            DeviceType::SpaceMousePlusXT => write!(f, "SpaceMouse Plus XT"),
+            DeviceType::ConnexionCadManUsb => write!(f, "3Dconnexion CadMan (USB version)"),
+            DeviceType::SpaceMouseClassic => write!(f, "SpaceMouse Classic"),
+            DeviceType::Spaceball5000Usb => write!(f, "Spaceball 5000 (USB version)"),
+            DeviceType::SpaceTraveller => write!(f, "Space Traveller"),
+            DeviceType::SpacePilot => write!(f, "Space Pilot"),
+            DeviceType::SpaceNavigator => write!(f, "Space Navigator"),
+            DeviceType::SpaceExplorer => write!(f, "Space Explorer"),
+            DeviceType::SpaceNavigatorForNotebooks => write!(f, "Space Navigator for Notebooks"),
+            DeviceType::SpacePilotPro => write!(f, "Space Pilot Pro"),
+            DeviceType::SpaceMousePro => write!(f, "SpaceMouse Pro"),
+            DeviceType::NuLOOQ => write!(f, "NuLOOQ"),
+            DeviceType::SpaceMouseWireless => write!(f, "SpaceMouse Wireless"),
+            DeviceType::SpaceMouseProWireless => write!(f, "SpaceMouse Pro Wireless"),
+            DeviceType::SpaceMouseEnterprise => write!(f, "SpaceMouse Enterprise"),
+            DeviceType::SpaceMouseCompact => write!(f, "SpaceMouse Compact"),
+            DeviceType::SpaceMouseModule => write!(f, "SpaceMouse Module"),
+        }
+    }
 }
--- libspnav/src/open.rs
+++ libspnav/src/open.rs
@@ -1,11 +1,12 @@
-use crate::device::Device;
+use crate::device::{Device, DeviceType};
 use crate::libspnav;
-use std::ffi::{c_char, c_int};
+use std::ffi::{c_char, c_int, c_uint};
 
 #[derive(Debug)]
 pub enum OpenError {
     Connect,
     RetrieveDeviceName,
+    RetrieveDeviceType,
     RetrieveDeviceAxes,
     RetrieveDeviceButtons,
 }
@@ -17,6 +18,7 @@ impl std::fmt::Display for OpenError {
         match self {
             OpenError::Connect => write!(f, "Failed to connect to the daemon!"),
             OpenError::RetrieveDeviceName => write!(f, "Failed to retrieve the device's name!"),
+            OpenError::RetrieveDeviceType => write!(f, "Failed to retrieve the device's type!"),
             OpenError::RetrieveDeviceAxes => write!(f, "Failed to retrieve the device's number of axes!"),
             OpenError::RetrieveDeviceButtons => write!(f, "Failed to retrieve the device's number of buttons!")
         }
@@ -40,6 +42,7 @@ pub fn open() -> Result<Device, OpenError> {
     Ok(
         Device {
             name: get_device_name()?,
+            ty: get_device_type()?,
             axes: get_device_axes()?,
             buttons: get_device_buttons()?,
         }
@@ -58,6 +61,41 @@ fn get_device_name() -> Result<String, OpenError> {
     }
 
     Ok(String::from_utf8_lossy(&buffer[..result as usize]).to_string())
+}
+
+fn get_device_type() -> Result<DeviceType, OpenError> {
+
+    let result = unsafe {
+        libspnav::spnav_dev_type() as c_uint
+    };
+
+    match result {
+        libspnav::SPNAV_DEV_UNKNOWN => Ok(DeviceType::Unknown),
+        libspnav::SPNAV_DEV_SB2003 => Ok(DeviceType::Spaceball2003),
+        libspnav::SPNAV_DEV_SB3003 => Ok(DeviceType::Spaceball3003),
+        libspnav::SPNAV_DEV_SB4000 => Ok(DeviceType::Spaceball4000),
+        libspnav::SPNAV_DEV_SM => Ok(DeviceType::MagellanSpaceMouse),
+        libspnav::SPNAV_DEV_SM5000 => Ok(DeviceType::Spaceball5000Serial),
+        libspnav::SPNAV_DEV_SMCADMAN => Ok(DeviceType::ConnexionCadManSerial),
+        libspnav::SPNAV_DEV_PLUSXT => Ok(DeviceType::SpaceMousePlusXT),
+        libspnav::SPNAV_DEV_CADMAN => Ok(DeviceType::ConnexionCadManUsb),
+        libspnav::SPNAV_DEV_SMCLASSIC => Ok(DeviceType::SpaceMouseClassic),
+        libspnav::SPNAV_DEV_SB5000 => Ok(DeviceType::Spaceball5000Usb),
+        libspnav::SPNAV_DEV_STRAVEL => Ok(DeviceType::SpaceTraveller),
+        libspnav::SPNAV_DEV_SPILOT => Ok(DeviceType::SpacePilot),
+        libspnav::SPNAV_DEV_SNAV => Ok(DeviceType::SpaceNavigator),
+        libspnav::SPNAV_DEV_SEXP => Ok(DeviceType::SpaceExplorer),
+        libspnav::SPNAV_DEV_SNAVNB => Ok(DeviceType::SpaceNavigatorForNotebooks),
+        libspnav::SPNAV_DEV_SPILOTPRO => Ok(DeviceType::SpacePilotPro),
+        libspnav::SPNAV_DEV_SMPRO => Ok(DeviceType::SpaceMousePro),
+        libspnav::SPNAV_DEV_NULOOQ => Ok(DeviceType::NuLOOQ),
+        libspnav::SPNAV_DEV_SMW => Ok(DeviceType::SpaceMouseWireless),
+        libspnav::SPNAV_DEV_SMPROW => Ok(DeviceType::SpaceMouseProWireless),
+        libspnav::SPNAV_DEV_SMENT => Ok(DeviceType::SpaceMouseEnterprise),
+        libspnav::SPNAV_DEV_SMCOMP => Ok(DeviceType::SpaceMouseCompact),
+        libspnav::SPNAV_DEV_SMMOD => Ok(DeviceType::SpaceMouseModule),
+        _ => Err(OpenError::RetrieveDeviceType)
+    }
 }
 
 fn get_device_axes() -> Result<u32, OpenError> {
