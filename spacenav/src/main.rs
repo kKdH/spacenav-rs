d1a85a8ce194e61bf362d5dd87289bedb5c15e0d
Elmar Schug <elmar.schug@jayware.org>
2026-03-06 10:52:53 +0100

    Added a window icon.

--- spacenav/src/main.rs
+++ spacenav/src/main.rs
@@ -1,11 +1,25 @@
 use crate::app::App;
+use iced::window;
+use iced::window::settings::PlatformSpecific;
+use image::ImageFormat;
 
 mod spnav;
 mod app;
 
 fn main() -> Result<(), iced::Error> {
+    let app_icon = include_bytes!("../assets/app-icon.webp");
     iced::application(App::default, App::update, App::view)
         .subscription(App::subscription)
         .title("SpaceNav")
+        .window(window::Settings {
+            platform_specific: PlatformSpecific {
+                application_id: String::from("spacenav"),
+                ..Default::default()
+            },
+            icon: Some(window::icon::from_file_data(app_icon, Some(ImageFormat::WebP))
+                .expect("Failed to load embedded app icon")
+            ),
+            ..Default::default()
+        })
         .run()
 }
